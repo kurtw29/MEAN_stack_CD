d2bba4285a5d0a0aece59af1e4c3a1e2c0b8e577
kurtw29 <ckurt37@gmail.com>
2018-09-06 19:46:45 -0700

    updated cars and cats Express assignment

--- Express/Hello_Express/cars_and_cats.js
+++ Express/Hello_Express/cars_and_cats.js
@@ -5,15 +5,6 @@ var app = express();
 app.use(express.static(__dirname + "/static"));
     console.log( __dirname);
 
-app.use(express.static(__dirname + "/static/cars.html"));
-    console.log( __dirname);
-
-app.use(express.static(__dirname + "/static/cats.html"));
-    console.log( __dirname);
-
-app.use(express.static(__dirname + "/static/form.html"));
-    console.log( __dirname);
-
 app.listen(8000, function(){
     console.log("listening on port 8000");
 })
