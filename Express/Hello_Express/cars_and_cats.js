a0360efd30c6b3176f5511d74728a74d0a2fda89
kurtw29 <ckurt37@gmail.com>
2018-09-06 20:05:40 -0700

    completed ejs cars and cat express

--- Express/Hello_Express/cars_and_cats.js
+++ Express/Hello_Express/cars_and_cats.js
@@ -2,8 +2,16 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static(__dirname + "/static"));
-    console.log( __dirname);
+app.use(express.static(__dirname + "/static/"));
+
+//tell the location where express will look for the ejs views
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+
+app.get("/cars", function(request, response){
+    response.render("cars");
+})
 
 app.listen(8000, function(){
     console.log("listening on port 8000");
