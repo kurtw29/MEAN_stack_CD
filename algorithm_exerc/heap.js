bc0d3846d03c2f6ff254ddcac96103ed09e6aab3
kurtw29 <ckurt37@gmail.com>
2018-09-06 18:38:40 -0700

    minor update & correction

--- algorithm_exerc/heap.js
+++ algorithm_exerc/heap.js
@@ -23,7 +23,7 @@ Heap.prototype.addToHeap = function (val){
 Heap.prototype.remove = function(){
     // Checking the [] and [undefine, val1] cases
     if(this.heap.length == 1){
-        this.heap.pop();
+        var extracted_value = this.heap.pop();
         return console.log("no more element")
     }else if(this.heap.length == 2){
         this.heap.pop();
@@ -34,11 +34,11 @@ Heap.prototype.remove = function(){
         this.heap[1] = this.heap[this.heap.length-1]
         this.heap[this.heap.length-1] = temp;
         // pop the last index of array
-        this.heap.pop();
+        var extracted_value = this.heap.pop();
         var p = 1;
         var c1 = p*2;
         var c2 = p*2+1;
-        console.log("\nbefore the while loop: ", this.heap)
+        // console.log("\nbefore the while loop: ", this.heap)
         var counter = 0;
         // Starting the "sinking/bubble down process"
         while( ((this.heap[c1] != undefined && this.heap[c2] == undefined) && 
@@ -47,8 +47,8 @@ Heap.prototype.remove = function(){
                 ((this.heap[c1] != undefined && this.heap[c2] != undefined) && ((this.heap[p] > this.heap[c1])|| this.heap[p] > this.heap[c2]))
         ){
                 counter++;
-                console.log("\nentered the while loop. Counter: ",counter)
-                console.log("heap[",p,"]:",this.heap[p], " heap[",c1,"]:",this.heap[c1], " heap[",c2,"]:",this.heap[c2])
+                // console.log("\nentered the while loop. Counter: ",counter)
+                // console.log("heap[",p,"]:",this.heap[p], " heap[",c1,"]:",this.heap[c1], " heap[",c2,"]:",this.heap[c2])
                     if(this.heap[p] > this.heap[c1]){
                         var temp = this.heap[p];
                         this.heap[p] = this.heap[c1];
@@ -65,22 +65,21 @@ Heap.prototype.remove = function(){
                         c2 = p*2+1
                 }
             }
-            console.log("finishing while",counter," loop")
-            console.log("heap[",p,"]:",this.heap[p], " heap[",c1,"]:",this.heap[c1], " heap[",c2,"]:",this.heap[c2])
+            // console.log("finishing while",counter," loop")
+            // console.log("heap[",p,"]:",this.heap[p], " heap[",c1,"]:",this.heap[c1], " heap[",c2,"]:",this.heap[c2])
        
         }
-        return this.heap;
+        return extracted_value;
 }
 
 var arr = new Heap()
 arr.addToHeap(5)
 console.log(arr);
-arr.remove();
+console.log(arr.remove(),"removed", arr);
 arr.addToHeap(5);
 arr.addToHeap(6);
 console.log(arr);
-arr.remove();
-console.log(arr);
+console.log(arr.remove(),"removed", arr);
 arr.addToHeap(7);
 arr.addToHeap(8);
 arr.addToHeap(9);
@@ -90,5 +89,4 @@ arr.addToHeap(15);
 arr.addToHeap(17);
 arr.addToHeap(21);
 console.log(arr);
-arr.remove();
-console.log(arr);
+console.log(arr.remove(),"removed:", arr);
