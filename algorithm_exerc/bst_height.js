8f58b0b1ff255620bca21fb72d52bdf679777cf7
kurtw29 <ckurt37@gmail.com>
2018-09-13 10:18:59 -0700

    binary search tree height function()

---
+++ algorithm_exerc/bst_height.js
@@ -0,0 +1,33 @@
+BST.prototype.height = function(){
+    if(this.root == null){
+        return 0;
+    }else{
+        var height_level = this.root.height();
+        return height_level;
+    }
+}
+
+BSTNode.prototype.height = function(){
+    if(this.left == null && this.right == null){
+        return 1
+    }
+
+    if(this.left != null){
+        var left = this.left.height();
+    }else{
+        var left = 0
+    }
+
+    if(this.right != null){
+        var right = this.right.height();
+    }else{
+        var right = 0
+    }
+
+    if(left > right){
+        return left++;
+    }else{
+        return right++;
+    }
+
+}
--- algorithm_exerc/testing.js
+++ algorithm_exerc/testing.js
@@ -53,6 +53,47 @@ BST.prototype.start_traverse = function(){
     }
 }
 
+// Finding the height of the BST:
+BST.prototype.height = function(){
+    //check the null case
+    if(this.root == null){
+        return 0;
+    //if not null, check the BSTNode's height function
+    }else{
+        var height_level = this.root.height();
+        return height_level;
+    }
+}
+
+BSTNode.prototype.height = function(){
+    // Check the end case
+    if(this.left == null && this.right == null){
+        return 1
+    }
+    // check the LEFT (lower number)
+    if(this.left != null){
+        // invoke the height function to check that node's height
+        var left = this.left.height();
+    }else{
+        //nothing at the left, so return 0;
+        var left = 0
+    }
+    // After checking left, check the RIGHT side (higher number).
+    if(this.right != null){
+        var right = this.right.height();
+    }else{
+        var right = 0
+    }
+    // After checking, we compare LEFT and RIGHT height, we return the higher height plus 1
+    if(left > right){
+        left++;
+        return left;
+    }else{
+        right++;
+        return right;
+    }
+
+}
 
 //testing here:
 var test = new BST();
@@ -67,5 +108,5 @@ test.insert(27);
 console.log('inserted 27')
 test.insert(68);
 console.log('inserted 68')
-
 test.start_traverse();
+console.log('height_level of BST:',test.height());
