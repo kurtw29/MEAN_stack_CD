b5affb3bdfa962c2cb295d96894a17326a608d55
kurtw29 <ckurt37@gmail.com>
2018-09-14 08:48:56 -0700

    dom manipulation restful task

--- Angular/HelloAngular/public/src/app/app.component.ts
+++ Angular/HelloAngular/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,24 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MEAN ';
-  constructor(private _httpService: HttpService){}
+  tasks;
+  secTask: any;
+
+  constructor(private _httpService: HttpService){
+  }
+
+  // ngOnInIt will run when the component is initialized, after the constructor method
+  ngOnInit(){
+    this.getTaskFromService();
+  }
+  getTaskFromService(){
+    let observable = this._httpService.getTask();
+    observable.subscribe(data =>{
+      console.log("Got our data!", data);
+      this.tasks = data;
+      this.secTask = data[1]
+    })
+  }
 }
--- Angular/HelloAngular/public/src/app/http.service.ts
+++ Angular/HelloAngular/public/src/app/http.service.ts
@@ -9,17 +9,19 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
   constructor(private _http: HttpClient) {
     //invoke the getTask function
-    this.getTask();
-    this.getTaskID('5b99ea51349ea663cdc7f9d3')
+    // this.getTask();
+    // this.getTaskID('5b99ea51349ea663cdc7f9d3')
   }
   // create function to fetch data
   getTask(){
-    let tempObs = this._http.get('/tasks');
-    tempObs.subscribe(data => console.log("Got Tasks. data", data))
+    // let tempObs = this._http.get('/tasks');
+    // tempObs.subscribe(data => console.log("Got Tasks. data", data))
+    return this._http.get('/tasks');
   }
   // create another function to fetch data by id
   getTaskID(id){
-    let tempObs = this._http.get('/tasks/'+id);
-    tempObs.subscribe(data => console.log("Got Task by ID data:", data))
+    // let tempObs = this._http.get('/tasks/'+id);
+    // tempObs.subscribe(data => console.log("Got Task by ID data:", data))
+    return this._http.get('/tasks/'+id);
   }
 }
